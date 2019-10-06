9e217f4e20e3904ac3a75422b2974a6e659a9b3b
robert <s15753@pjwstk.edu.pl>
2019-10-06 12:36:18 +0200

    first test

--- baseny_lab1/src/main/java/s15753/tau/labone/domain/Pool.java
+++ baseny_lab1/src/main/java/s15753/tau/labone/domain/Pool.java
@@ -1,18 +1,28 @@
+package s15753.tau.labone.domain;
 
-
-class Pool {
+public class Pool {
     protected int id;
     protected Double length;
     protected Double width;
     protected Double depth;
     
-    void Pool(int id, Double length, Double width, Double depth){
+    public Pool(int id, Double length, Double width, Double depth){
         this.id = id;
         this.length = length;
         this.width = width;
         this.depth = depth;
     }
 
+    public void changeLength(Double length){
 
+    }
+
+    public void changeWidth(Double width){
+
+    }
+
+    public void changeDepth(Double depth){
+        
+    }
 
 }
---
+++ baseny_lab1/src/test/java/s15753/tau/labone/domain/PoolTest.java
@@ -0,0 +1,22 @@
+ 
+package s15753.tau.labone.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(JUnit4.class)
+public class PoolTest {
+
+
+    @Test
+    public void poolConstructorCanAcceptNumberOfDimensionsTest() {
+        assertNotNull(new Pool(1, 2.2, 3.3, 4.4));
+    }
+
+   
+
+}
