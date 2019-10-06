c75c517b1192da525b70bc91403d00fcf86fde70
robert <s15753@pjwstk.edu.pl>
2019-10-06 11:55:10 +0200

    Pool constructor and change methods

--- baseny_lab1/src/main/java/s15753/tau/labone/domain/Pool.java
+++ baseny_lab1/src/main/java/s15753/tau/labone/domain/Pool.java
@@ -1,5 +1,18 @@
 
 
 class Pool {
+    protected int id;
+    protected Double length;
+    protected Double width;
+    protected Double depth;
     
+    void Pool(int id, Double length, Double width, Double depth){
+        this.id = id;
+        this.length = length;
+        this.width = width;
+        this.depth = depth;
+    }
+
+
+
 }
