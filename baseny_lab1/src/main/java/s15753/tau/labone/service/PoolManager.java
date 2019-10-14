406fac4b0411e580962275211e66fd8df62567ef
robert <s15753@pjwstk.edu.pl>
2019-10-14 22:06:53 +0200

    Remove interface

--- baseny_lab1/src/main/java/s15753/tau/labone/service/PoolManager.java
+++ baseny_lab1/src/main/java/s15753/tau/labone/service/PoolManager.java
@@ -1,10 +1,10 @@
 package s15753.tau.labone.service;
 
-public interface PoolManager {
-
-    public void addPool(int id, Double length, Double width, Double depth);
-    public Pool getPool(int id);
-    public void updatePool(int id, Double length, Double width, Double depth);
-    public void deletePool(int id);
-
-}
+//public interface PoolManager {
+//
+//    public void addPool(int id, Double length, Double width, Double depth);
+//    public Pool getPool(int id);
+//    public void updatePool(int id, Double length, Double width, Double depth);
+//    public void deletePool(int id);
+//
+//}
