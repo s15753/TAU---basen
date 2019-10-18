13f3d563008f8555430f281d9b76007921e0ca4e
robert <s15753@pjwstk.edu.pl>
2019-10-18 21:27:12 +0200

    storage create

--- baseny_lab1/src/main/java/s15753/tau/labone/service/PoolStorageImpl.java
+++ baseny_lab1/src/main/java/s15753/tau/labone/service/PoolStorageImpl.java
@@ -10,5 +10,13 @@ public class PoolStorageImpl {
         this.storage = new HashMap<Integer,Pool>();
     }
 
+    public void create(Pool pool) throws Exception {
+        if(this.storage.containsKey(pool.getId())
+        {
+            throw new Exception("Id already added");
+        }
+        this.storage.put(pool.getId(), pool);
+    }
+
 
 }
--- baseny_lab1/src/test/java/s15753/tau/labone/service/StorageTest.java
+++ baseny_lab1/src/test/java/s15753/tau/labone/service/StorageTest.java
@@ -10,9 +10,23 @@ import static org.hamcrest.CoreMatchers.*;
 @RunWith(JUnit4.class)
 public class StorageTest {
 
+    @Before
+    public void setUp(){
+        Poll pool_1 = new Pool(1, 2.0, 3.0, 4.0);
+        PoolStorageImpl storage = new PoolStorageImpl();
+        storage.put(1, pool_1);
+    }
+
     @Test
     public void storageConstructorCanAcceptNumberOfDimensionsTest() {
         assertNotNull(new PoolStorageImpl());
+    }
+
+    @Test
+    public void addEntryToStorageTest() {
+        Pool pool_2 = new Pool(2, 4.0, 5.0, 6.0);
+        storage.create(pool_2);
+        assertTrue(storage.get(2).getId == 2);
     }
 
 
