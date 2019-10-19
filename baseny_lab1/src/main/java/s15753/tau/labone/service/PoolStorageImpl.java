000855dcdca9283105ada8579209fd60327c850d
robert <s15753@pjwstk.edu.pl>
2019-10-19 21:03:47 +0200

    storage create

--- baseny_lab1/src/main/java/s15753/tau/labone/service/PoolStorageImpl.java
+++ baseny_lab1/src/main/java/s15753/tau/labone/service/PoolStorageImpl.java
@@ -7,14 +7,14 @@ import s15753.tau.labone.domain.Pool;
 public class PoolStorageImpl {
     private ArrayList<Pool> storage = new ArrayList<Pool>();
 
-
+    storage.add(new Pool(1, 2.0, 3.0, 4.0));
 
     public void create(Pool pool) throws Exception {
         for(Pool p: storage ) {
-            if(p.getId() == pool.getId())
-            {
+            if (p.getId() == pool.getId()) {
                 throw new IllegalArgumentException();
             }
+        }
         storage.add(pool);
     }
 
@@ -33,10 +33,10 @@ public class PoolStorageImpl {
 
     public void update(int id, Double length, Double width, Double depth) throws Exception {
             for(Pool p: storage ) {
-                if(p.getId() == id)
-                {
+                if (p.getId() == id) {
                     throw new IllegalArgumentException();
                 }
+            }
         Pool updatedPool = storage.get(id);
         updatedPool.setLength(length);
         updatedPool.setWidth(width);
